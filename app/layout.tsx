c0ca4c57a74ebdffdf2e98137efb83e807a799ba
Zsolis-2028 <Zachary.Solis@hallmark.edu>
2026-06-24 17:00:55 -0500

    feat: add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-"use client";
-
 import type { ReactNode } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -8,6 +7,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <body suppressHydrationWarning={true}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
